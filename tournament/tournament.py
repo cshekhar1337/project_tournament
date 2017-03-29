151d6be83747d29b84519bdd0ce0fd71c171b311
cshekhar1337 <cshekhar@Chandras-MacBook-Pro.local>
2017-03-29 01:41:19 -0700

    first commit

---
+++ tournament/tournament.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# 
+# tournament.py -- implementation of a Swiss-system tournament
+#
+
+import psycopg2
+
+
+def connect():
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
+
+
+def deleteMatches():
+    """Remove all the match records from the database."""
+    con = connect()
+    
+    query = "delete from matches"
+   
+    cur = con.cursor()
+    cur.execute(query)
+    con.commit()
+    cur.close()
+
+
+def deletePlayers():
+    """Remove all the player records from the database."""
+    con = connect()
+    
+    query = "delete from players"
+   
+    cur = con.cursor()
+    cur.execute(query)
+    con.commit()
+    cur.close()
+
+
+def countPlayers():
+    """Returns the number of players currently registered."""
+    con = connect()
+    
+    query = "select count(name) from players"
+
+    cur = con.cursor()
+    cur.execute(query)
+    val = cur.fetchone()
+    con.commit()
+    cur.close()
+    return val[0]
+
+
+
+def registerPlayer(name):
+    """Adds a player to the tournament database.
+  
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+  
+    Args:
+      name: the player's full name (need not be unique).
+    """
+    con = connect()
+    
+    query = "INSERT INTO players (name) VALUES (%s)"
+    val = (name ,)
+    cur = con.cursor()
+    cur.execute(query,val)
+    con.commit()
+    cur.close()
+
+
+
+def playerStandings():
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place, or a player
+    tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+    con = connect()
+    # join query on the two tables to get standing of each player. Used grouping and aggregate function to get wins.
+    query1 = '''
+    select t2.id, t2.name, COALESCE(t1.win_count, 0) as wins, t2.no_matches  from 
+    (SELECT id1, count(id1) as win_count from matches group by id1 order by 2 desc) as t1
+    RIGHT JOIN 
+    (select id, name, no_matches from players) as t2 
+    on t1.id1 = t2.id or t1 IS NULL order by wins
+    '''
+
+    cur = con.cursor()
+    #print(cur.mogrify(query1))
+    cur.execute(query1)
+    con.commit()
+    resList = cur.fetchall()
+    cur.close()
+    return resList
+
+    
+
+    
+
+
+def reportMatch(winner, loser):
+    """Records the outcome of a single match between two players.
+
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+    con = connect()
+    
+    query1 = "INSERT INTO matches (id1, id2) VALUES (%s , %s)"
+    val1 = (winner , loser)
+    cur = con.cursor()
+    cur.execute(query1, val1)
+    con.commit()
+    cur.close()
+    query2 = "UPDATE players SET no_matches = no_matches + 1 where id = %s or id = %s " # query to update players table to modify no of matches the player has played
+    val2 = (winner, loser)
+    cur = con.cursor()
+    #print(cur.mogrify(query2,val2))
+    cur.execute(query2, val2)
+    con.commit()
+    cur.close()
+
+
+
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
+  
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+  
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
+
+    standings = playerStandings() # use standing to get player standing in current round
+    result = []
+    i = 0
+    
+    while i < len(standings):
+# player1, player2 are grouped together as win_count is in the descending order. so players with winning equal or close are adjacent in the list
+        player1_details, player2_details = standings[i], standings[i+1] 
+        i+= 2
+       
+        player1_id , player1_name = player1_details[0], player1_details[1]
+        player2_id, player2_name = player2_details[0], player2_details[1]
+       
+        result.append((player1_id, player1_name, player2_id, player2_name))
+    return result
+
+        
+
+
+
+
+
+
+
+
---
+++ tournament/tournament.sql
@@ -0,0 +1,53 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+CREATE DATABASE tournament; -- A database named tournament is created
+
+--\connect tournament;  
+CREATE SEQUENCE serial1 START 200; --create a sequence which generates value greater than 200
+
+
+CREATE TABLE players(         -- this table stores record of each player which includes his playerid, name, no of matches
+    id INT4 DEFAULT nextval('serial1'),
+    name varchar(50),
+    no_matches INT DEFAULT 0,
+    
+   PRIMARY KEY( id )
+);
+
+CREATE TABLE matches( -- this table records the matches. id1 -> winner id, id2 -> loser id
+	id1 INT references players(id), -- here id1 is foreign key which references players id
+	id2 INT references players(id) 
+);
+
+
+
+
+
+CREATE or Replace Function updatePlayer() RETURNS TRIGGER AS $$ -- this function executes when trigger is executed
+BEGIN
+Update players set no_matches = 0 where id =OLD.id1 or id = OLD.id2;
+RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER updateMatches AFTER DELETE on   -- this is a trigger which updates no_of_matches when matches are deleted
+matches 
+FOR EACH ROW 
+EXECUTE PROCEDURE updatePlayer();
+
+
+
+
+
+
+
+
+
+
+
